4a4f897a161a24dfa36ba150742d90dac2087f01
Aish13-12 <54772803+Aish13-12@users.noreply.github.com>
2020-10-10 22:23:31 +0530

    Add files via upload

--- prog2.asm
+++ prog2.asm
@@ -1,8 +1,8 @@
-# STRCPY.
+# STRCPY. 
 addi  $sp, $sp, 4
 sw    $s0, 0($sp)
 add   $s0, $zero, $zero
-# L1:(address->96)
+# L1:(address with offset->96)
 add   $t1, $s0, $a1
 lb    $t2, 0($t1)
 add   $t3, $s0, $a0
@@ -10,7 +10,7 @@ sb    $t2, 0($t3)
 beq   $t2, $zero, 120
 addi  $s0, $s0, 1
 j     96
-# L2:(address->120)
+# L2:(->120)
 lw    $s0, 0($sp)
 addi  $sp, $sp, 4
 jr    $ra
